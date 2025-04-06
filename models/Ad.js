2c72bc5c70751f3f5ee1d6c719e1a22692af7d3f
Albert-Rangel <albertsleyther@gmail.com>
2025-04-06 10:45:29 -0400

    a

--- models/Ad.js
+++ models/Ad.js
@@ -17,12 +17,8 @@ adSchema.virtual('fullImageUrl').get(function() {
   // Si ya es URL absoluta (http/https)
 
 
-  console.log("entro en afullImageUrl")
-  console.log("startsWith('https')1" +  this.imageUrl)
-
   if (this.imageUrl?.startsWith('https')) {
-    console.log("startsWith('https')2" +  this.imageUrl)
-
+   
     return this.imageUrl;
   }
   
@@ -37,16 +33,11 @@ adSchema.virtual('fullImageUrl').get(function() {
       ? this.imageUrl 
       : `/${this.imageUrl}`;
 
-      console.log("producion")
-      // console.log("`${baseUrl}${imagePath}`:" + `${baseUrl}${imagePath}`)
-      console.log("`{imagePath}`:" + this.imageUrl)
-    
-    // return `${baseUrl}${imagePath}`;
-     return this.imageUrl;
+    return `${baseUrl}${imagePath}`;
+    //  return this.imageUrl;
   }
   
-  console.log("desarrollo")
-  console.log("this.imageUrl3:"+ this.imageUrl)
+  
   // En desarrollo, usa ruta relativa
   return this.imageUrl;
 });
